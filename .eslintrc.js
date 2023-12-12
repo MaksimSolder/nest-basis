99a422b595807b99d7235890c5e2cdf3ab6e3551
MaksSolder <fak8668@gmail.com>
2023-12-12 21:28:23 +0300

    Refactor all

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
+    tsconfigRootDir : __dirname,
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
@@ -17,9 +17,30 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    "no-empty-function": "off",
+    "no-empty-pattern": "off",
+    "prettier/prettier": ["error",
+      {
+        "singleQuote": true,
+        "useTabs": true,
+        "semi": true,
+        "trailingComma": "all",
+        "bracketSpacing": true,
+        "printWidth": 100,
+        "endOfLine": "auto"
+      }
+    ],
+    "react-hooks/exhaustive-deps": "off",
+    "@typescript-eslint/no-empty-function": [
+      "off"
+    ],
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-namespace": "off",
+    "@typescript-eslint/ban-ts-comment": "off",
+    "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/ban-types": "off"
   },
 };
---
+++ src/top-page/toppage.model.ts
@@ -0,0 +1,28 @@
+export class TopPageModel {
+    firstCategory: TopLevelCategory;
+    secondCategory: string;
+    title: string;
+    category: string;
+    hh?: {
+        count: number;
+        juniorSalary: number;
+        middleSalary: number;
+        seniorSalary: number;
+    };
+    advantages: {
+        title: string;
+        description: string;
+    } [];
+    seoText: string;
+    tagsTitle: string;
+    tags: string[];
+}
+
+export enum TopLevelCategory {
+    Courses,
+    Services,
+    Books,
+    Products
+}
+
+
